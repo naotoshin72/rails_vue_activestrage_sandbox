c0c2854a6dc1803b8fec2bdd5bfe876a2fecbaf2
naotoshin72 <naotoshi-nishijima@cuebic.co.jp>
2018-04-22 22:15:00 +0900

    起動シェルと設定を修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,26 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import Vue from 'vue'
+var vms = []
 
-console.log('Hello World from Webpacker')
+var options = {}
+
+const requireContext = require.context('./options', false, /\.js$/)
+requireContext.keys().forEach(key => {
+  let name = key.split('/').pop().split('.').shift()
+  options[name] = requireContext(key).default
+})
+document.addEventListener('turbolinks:load', () => {
+  let templates = document.querySelectorAll('[data-vue]')
+  for (let el of templates) {
+    let vm = new Vue(
+      Object.assign(options[el.dataset.vue], { el })
+    )
+    vms.push(vm)
+  }
+})
+
+document.addEventListener('turbolinks:visit', () => {
+  for (let vm of vms) {
+    vm.$destroy()
+  }
+  vms = []
+})
